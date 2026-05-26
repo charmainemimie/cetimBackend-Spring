923a97f6cd68bc4457c3289271cad7369aee9813
charmainemimie <charmainemangorima@gmail.com>
2026-05-26 23:37:21 +0200

    initial setup

---
+++ src/main/java/com/mimie/cetimBackend/CetimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mimie.cetimBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CetimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CetimBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mimie/cetimBackend/CetimBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mimie.cetimBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CetimBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
